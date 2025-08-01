b1bfce71a20ca1d94f2d636e265d090b22ca562e
959AI994 <115469462+959AI994@users.noreply.github.com>
2025-08-01 14:52:59 +0800

    feat: update code.

--- code/shared-park/src/main/java/com/lbk/sharper/config/WechatConfig.java
+++ code/shared-park/src/main/java/com/lbk/sharper/config/WechatConfig.java
@@ -7,12 +7,12 @@ package com.lbk.sharper.config;
  * @Version 1.0
  */
 public class WechatConfig {
-//    public static String APPID = "wx32db5b24073bc2ff";
-//    public static String SECRET = "105ec7a5bdf93ca94107cd38ba7756cf";
+//    public static String APPID = "wx32db5b240xxx";
+//    public static String SECRET = "105exxx";
 //    public static String grant_type = "authorization_code";
 
-    public static String APPID = "wxaf23c8ac13169030";
-    public static String SECRET = "a9d326fa6b71c36bb3825a2854a79d43";
+    public static String APPID = "wxaxxx";
+    public static String SECRET = "xxxxxxx";
     public static String grant_type = "authorization_code";
 
 }
